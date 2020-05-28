bd588a21bfe4608b96c21af17dfd81b8e62468e4
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-28 16:44:27 +0200

    added timing overview

--- profiling/profiling.py
+++ profiling/profiling.py
@@ -1,6 +1,6 @@
 import os
-import pytest
-from matchms import calculate_scores, Scores
+from timeit import default_timer as timer
+from matchms import Scores
 from matchms.filtering import add_parent_mass
 from matchms.filtering import default_filters
 from matchms.filtering import normalize_intensities
@@ -20,22 +20,37 @@ def apply_my_filters(s):
     s = require_minimum_number_of_peaks(s, n_required=5)
     return s
 
+
+log = [(timer(), "start")]
 module_root = os.path.join(os.path.dirname(__file__), "..")
-spectrums_file = os.path.join("GNPS-LIBRARY.mgf")
+
+spectrums_file = os.path.join(module_root, "tests", "pesticides.mgf")
+log.append((timer(), "end load file"))
 
 # apply my filters to the data
 spectrums = [apply_my_filters(s) for s in load_from_mgf(spectrums_file)]
+log.append((timer(), "end filtering"))
 
 # omit spectrums that didn't qualify for analysis
 spectrums = [s for s in spectrums if s is not None]
+log.append((timer(), "end omit None"))
 
 # define similarity function
 cosine_greedy = CosineGreedy()
 
 # this will be a library grouping analysis, so queries = references = spectrums
-queries = spectrums[:]
-references = spectrums[:]
+queries = references = spectrums
 
-similarity_matrix = Scores(references=5*references,
-                           queries=5*queries,
-                           similarity_function=cosine_greedy).calculate().scores
+log.append((timer(), "start problem definition"))
+problem_definition = Scores(references=references, queries=queries, similarity_function=cosine_greedy)
+log.append((timer(), "end problem definition"))
+
+log.append((timer(), "start calculating"))
+problem_definition.calculate()
+log.append((timer(), "end calculating"))
+
+log.append((timer(), "end"))
+
+for index, (t, msg) in enumerate(log):
+    delta_t = t - log[index - 1][0] if index > 0 else 0
+    print("{0:10.3f}: {1}".format(delta_t, msg))
