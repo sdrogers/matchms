ab04dd10b0ee01acf161e1b80cfdc6fd7ad1ff21
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-29 13:29:42 +0200

    Spikes object now has immutable .mz and .intensities members

--- matchms/Spikes.py
+++ matchms/Spikes.py
@@ -6,6 +6,7 @@ class Spikes:
     Stores arrays of intensities and M/z values, with some checks on their internal consistency.
     """
     def __init__(self, mz=None, intensities=None):
+
         assert isinstance(mz, numpy.ndarray), "Input argument 'mz' should be a numpy.array."
         assert isinstance(intensities, numpy.ndarray), "Input argument 'intensities' should be a numpy.array."
         assert mz.shape == intensities.shape, "Input arguments 'mz' and 'intensities' should be the same shape."
@@ -14,34 +15,40 @@ class Spikes:
 
         self._mz = mz
         self._intensities = intensities
+        self._make_immutable()
 
         assert self._is_sorted(), "mz values are out of order."
 
     def __eq__(self, other):
         return \
-            self.mz.shape == other.mz.shape and \
-            numpy.allclose(self.mz, other.mz) and \
+            self._mz.shape == other.mz.shape and \
+            numpy.allclose(self._mz, other.mz) and \
             self.intensities.shape == other.intensities.shape and \
-            numpy.allclose(self.intensities, other.intensities)
+            numpy.allclose(self._intensities, other.intensities)
 
     def __len__(self):
         return self._mz.size
 
     def __getitem__(self, item):
-        return [self.mz, self.intensities][item]
+        return [self._mz, self._intensities][item]
 
     def _is_sorted(self):
-        return numpy.all(self.mz[:-1] <= self.mz[1:])
+        return numpy.all(self._mz[:-1] <= self._mz[1:])
+
+    def _make_immutable(self):
+        self._mz.setflags(write=False)
+        self._intensities.setflags(write=False)
+        return self
 
     def clone(self):
-        return Spikes(self.mz, self.intensities)
+        return Spikes(self._mz.copy(), self._intensities.copy())
 
     @property
     def mz(self):
         """getter method for mz private variable"""
-        return self._mz.copy()
+        return self._mz
 
     @property
     def intensities(self):
         """getter method for intensities private variable"""
-        return self._intensities.copy()
+        return self._intensities
